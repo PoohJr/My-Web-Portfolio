abc19e9363923bba7003aa04345ba775d9b4320e
PoohJr <Williamdomenech06@gmail.com>
2024-05-26 15:22:20 -0400

    started

---
+++ myport/src/App.js
@@ -0,0 +1,14 @@
+import Preloader from "./component/Preloader";
+
+
+
+function App() {
+  return (
+<>
+<Preloader/>
+    <p>Hello</p>
+</>
+  );
+}
+
+export default App;
---
+++ myport/src/component/Preloader.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './preloader.css'
+
+const Preloader = () =>{
+    return(<>
+    <div className="container">
+        <div className="item">
+            <span>Devloper,</span>
+            <span>Devloper,</span>
+            <span>Devloper,</span>
+        </div>
+    </div>
+    </>)
+}
+
+export default Preloader
